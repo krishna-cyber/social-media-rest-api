ca66143784fb6b29745788920dc4c923851e1d6b
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-12-01 15:56:40 +0545

    user update route added

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,14 @@ router.get("/", (req, res) => {
 
 //update a user
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId || req.body.isAdmin) {
     if (req.body.password) {
       try {
+        const { password } = req.body;
         const salt = await bcrypt.genSalt(10);
-        req.body.password = await bcrypt.hash(req.body.password, salt);
-      } catch (err) {
-        return res.status(500).json(err);
+        req.body.password = await bcrypt.hash(password, salt);
+      } catch (error) {
+        res.status(500).json(error);
       }
     }
     try {
@@ -22,11 +23,11 @@ router.put("/:id", async (req, res) => {
         $set: req.body,
       });
       res.status(200).json("Account has been updated");
-    } catch (err) {
-      return res.status(500).json(err);
+    } catch (error) {
+      res.status(500).json(error);
     }
   } else {
-    return res.status(403).json("You can update only your account!");
+    return res.status(403).send("You can update only your account!");
   }
 });
 //delete a user
