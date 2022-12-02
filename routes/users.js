c92d5677218c5a09a54bf67b4e9d0880750b7d75
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-12-02 20:40:55 +0545

    user get  api created and tested it works fine

--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,35 @@ router.delete("/:id", async (req, res) => {
   }
 });
 //get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //follow a user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+      } else {
+        res.status(403).json("You already follow this user");
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You can't follow yourself");
+  }
+});
+
 //unfollow a user
 
 module.exports = router;
