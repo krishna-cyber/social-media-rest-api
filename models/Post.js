6be0a6e55d7ba86b6d642be81d7437fa58d7c21c
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-12-03 11:05:22 +0545

    model created for post

---
+++ models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+//creating user schema
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      max: 500,
+    },
+    img: {
+      type: String,
+    },
+    likes: {
+      type: Array,
+      default: [],
+    },
+  },
+
+  { timestamps: true }
+);
+
+//exporting models to use in other files
+module.exports = mongoose.model("Post", userSchema);
