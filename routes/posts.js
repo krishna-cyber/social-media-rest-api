37a367db64d64b33263ea2269401f8618daafe3d
krishna tiwari <tiwarikrishna54321@gmail.com>
2022-12-04 20:02:24 +0545

    post create route created

--- routes/posts.js
+++ routes/posts.js
@@ -6,6 +6,15 @@ router.get("/", async (req, res) => {
 });
 
 //create a post
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //update a post
 //delete a post
 //like a post
